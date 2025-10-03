be0fb1b358a3b691caf40cb8241b0feda4dcc383
Vero <v.arriola@ciencias.unam.mx>
2025-10-02 22:25:16 -0600

    xacro de paquito base

---
+++ launch/viz_launch_paquito.py
@@ -0,0 +1,55 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+from launch_ros.descriptions import ParameterValue
+from launch.substitutions import Command
+
+def generate_launch_description():
+    package_dir = get_package_share_directory('viz_package_cpp')
+    path_to_urdf = os.path.join(package_dir, 'urdf', 'paquito.urdf')
+    #with open(path_to_urdf, 'r') as f:
+    #    robot_desc = f.read()
+    robot_desc = ParameterValue(
+                    Command(['xacro ', str(path_to_urdf)]), value_type=str
+                )
+    return LaunchDescription([
+        Node(
+            package='viz_package_cpp',
+            namespace='paquito1',
+            executable='viz_node',
+            name='viz',
+            output='screen',
+        ),
+        # Publica las transformaciones estáticas del modelo
+        Node(
+            package='robot_state_publisher',
+            name='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'robot_description': robot_desc,
+                'publish_frequency': 30.0,
+            }]
+        ),
+        # Publica las transformaciones de las articulaciones
+        Node(
+            package='joint_state_publisher',
+            name='joint_state_publisher',
+            executable='joint_state_publisher',
+            output='screen',
+            parameters=[{
+                'robot_description': robot_desc,
+                'publish_frequency': 30.0,
+            }]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', os.path.join(package_dir, 'rviz', 'panel.rviz')],
+            output='screen'
+        )
+    ])
+
